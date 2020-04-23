f782c4c7e5ab24ad29139e4e347360fd30d6d2f5
Mzem <mzemni@octo.com>
2020-04-23 14:50:43 +0200

    add rooms filesystem repo

--- src/controllers/roomsController.js
+++ src/controllers/roomsController.js
@@ -1,15 +1,17 @@
 const getAvailableRooms = require('../usecases/getAvailableRooms')
 const RoomsRepository = require('../repositories/roomsRepository')
+const RoomsFileSystemRepository = require('../repositories/roomsFileSystemRepository')
 
 module.exports = {
   findAvailableRooms(checkinDate, checkoutDate, guestsCount) {
     const roomsRepository = new RoomsRepository()
+    const roomsFileSystemRepository = new RoomsFileSystemRepository()
 
     return getAvailableRooms(
       checkinDate,
       checkoutDate,
       guestsCount,
-      roomsRepository
+      roomsFileSystemRepository
     )
   },
 }
